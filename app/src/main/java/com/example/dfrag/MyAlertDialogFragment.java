e5a2a631b130ac22f2cbac2fa95dbe6a2ec38e74
ameyaoka <ameyaoka444@gmail.com>
2020-02-19 12:56:58 +0530

    added file list to listfragment, full paths are to stored separate and  filenames are to be displayed

--- app/src/main/java/com/example/dfrag/MyAlertDialogFragment.java
+++ app/src/main/java/com/example/dfrag/MyAlertDialogFragment.java
@@ -3,16 +3,27 @@ package com.example.dfrag;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
+
+
 
 
 public class MyAlertDialogFragment extends DialogFragment
 {
 ArrayList selectedItems;
-    final CharSequence[] items = {"Red", "Green", "Blue"};
+
+
+    ArrayList<String> filenem = new ArrayList<String>();
+
+
+
     public MyAlertDialogFragment() {
 
         // Empty constructor required for DialogFragment
@@ -38,7 +49,40 @@ ArrayList selectedItems;
 
 
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    public Dialog onCreateDialog(Bundle savedInstanceState)
+    {
+   ////  ensure that the follwing line is added in AndroidManifest.xml
+   //// <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+
+        String tempstr;
+        String rootDir= Environment.getExternalStorageDirectory().getPath();
+        List<String> listItems = new ArrayList<String>();
+        File mfile=new File(rootDir);
+        File[] list=mfile.listFiles();
+        String tempupper;
+        for(int i=0;i<mfile.listFiles().length;i++)
+        {
+            tempstr=list[i].getAbsolutePath();
+            tempupper=tempstr.toUpperCase();
+            if(tempupper.endsWith(".BCH") )
+                listItems.add(list[i].getAbsolutePath());
+        }
+
+        final CharSequence[] items = listItems.toArray(new CharSequence[listItems.size()]);
+
+
+
+        filenem.add("red");
+        filenem.add("blue");
+        filenem.add("green");
+
+        String[] arr = new String[filenem.size()];
+        for(int i=0 ; i< filenem.size();i++){
+            arr[i] = filenem.get(i);
+            //getProductName or any suitable method
+        }
+
+
         selectedItems = new ArrayList();  // Where we track the selected items
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         // Set the dialog title
@@ -80,6 +124,39 @@ ArrayList selectedItems;
         return builder.create();
     }
 
+
+
+
+    void OpenFileDialog()
+    {	String tempstr;
+        String rootDir= Environment.getExternalStorageDirectory().getPath();
+        List<String> listItems = new ArrayList<String>();
+        File mfile=new File(rootDir);
+        File[] list=mfile.listFiles();
+        String tempupper;
+        for(int i=0;i<mfile.listFiles().length;i++)
+        {
+            tempstr=list[i].getAbsolutePath();
+            tempupper=tempstr.toUpperCase();
+            if(tempupper.endsWith(".BCH") )
+                listItems.add(list[i].getAbsolutePath());
+        }
+
+        final CharSequence[] items = listItems.toArray(new CharSequence[listItems.size()]);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Select File To Open...");
+        builder.setItems(items, new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int item)
+            {String ttt= (String) items[item];
+
+                Toast.makeText(getActivity(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
 
 
 
